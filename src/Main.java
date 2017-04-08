b0c4fea056196f931cda4e8d808d24e90dd03815
gavin <294830054@qq.com>
2017-04-08 17:49:02 +0800

    learn lambda

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
-import java.util.function.BinaryOperator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BinaryOperator<Integer> add = (x,y) -> x + y;
+        //BinaryOperator<Integer> add = (x,y) -> x + y;
 
+        //Runnable hello = () -> System.out.println("Hello,world!");
 
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        Stream<Integer> stem = list.stream().filter(item -> item % 2 != 0);
+        //long count = stem.count();
+        //System.out.println("list count:"+ count);
+
+        stem.map(item -> item * 2).forEach(item -> System.out.println(item));
     }
 
 
