115d4f00ebc616acfeeb60d87ad03199bee53b0d
gavin <294830054@qq.com>
2017-04-08 16:18:21 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.function.BinaryOperator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinaryOperator<Integer> add = (x,y) -> x + y;
+
+
+    }
+
+
+}
